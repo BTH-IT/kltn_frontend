984cf8429d53946944d81980ddfe4087b9f75dcd
BTH-IT <hungbienthanh@gmail.com>
2024-12-08 10:08:41 +0000

    update

--- renderer/app/layout.tsx
+++ renderer/app/layout.tsx
@@ -14,16 +14,16 @@ export const metadata: Metadata = {
   description: 'Based on Google Classroom',
   manifest: '/web.manifest',
   icons: {
-    icon: '/images/logo-2.png',
-    shortcut: '/images/logo-2.png',
-    apple: '/images/logo-2.png',
+    icon: 'https://bthung.id.vn/images/logo-2.png',
+    shortcut: 'https://bthung.id.vn/images/logo-2.png',
+    apple: 'https://bthung.id.vn/images/logo-2.png',
   },
   openGraph: {
     title: 'Courseroom Application',
     description: 'Based on Google Classroom',
     images: [
       {
-        url: '/images/shared-image.png',
+        url: 'https://bthung.id.vn/images/shared-image.png',
         width: 1200,
         height: 630,
         alt: 'Courseroom Preview Image',
